b534eb3d5b0bb52751af65e55cf7716f46c27edd
rdrdread <34808134+rdrdread@users.noreply.github.com>
2018-05-24 06:28:50 +0900

    Update kiwoom.py

--- kiwoom.py
+++ kiwoom.py
@@ -21,6 +21,25 @@ class MyWindow(QMainWindow):
     # 윈도우의 좌표, 사이즈를 변경하는 메소드
     self.setGeometry(300, 300, 300, 400)
     
+    self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+    
+    btn1 = QPushButton("Login", self)
+    btn1.move(20, 20)
+    btn1.clicked.connect(self.btn1_clicked)
+    
+    btn2 = QPushButton("Check state", self)
+    btn2.move(20, 70)
+    btn2.clicked.connect(self.btn2_clicked)
+    
+  def btn1_clicked(self):
+    ret = self.kiwoom.dynamicCall("CommConnect()")
+      
+  def btn2_clicked(self):
+    if self.kiwoom.dynamicCall("GetConnectState()") == 0:
+      self.statusBar().showMessage("Not connected")
+    else:
+      self.statusBar().showMessage("Connected")
+      
 if __name__ == "main":
   # QApplication 클래스의 인스턴스 생성
   # 이벤트 루프를 담당하는 부분
